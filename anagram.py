d93878887201eabb033bcd0da05e2d1d614841d3
Safonov Vladimir <vladimir_safonov@ukr.net>
2022-04-16 20:52:48 +0300

    ver. 1.0

--- anagram.py
+++ anagram.py
@@ -1,27 +1,38 @@
-def revers(text):
-    list_res = list(text)
-    i = 0
-    j = len(text) - 1
-    while i < j:
-        if not list_res[i].isalpha():
-            i += 1
-        elif not list_res[j].isalpha():
-            j -= 1
-        else:
-            new_char = list_res[i]
-            list_res[i] = list_res[j]
-            list_res[j] = new_char
-            i += 1
-            j -= 1
-    return ''.join(list_res)
+# def revers(text):
+#     list_res = list(text)
+#     i = 0
+#     j = len(text) - 1
+#     # print(list_res)
+#     while i < j:
+#         if not list_res[i].isalpha():
+#             i += 1
+#         elif not list_res[j].isalpha():
+#             j -= 1
+#         else:
+#             new_char = list_res[i]
+#             list_res[i] = list_res[j]
+#             list_res[j] = new_char
+#             i += 1
+#             j -= 1
+#     return ''.join(list_res)
+
+# def make_list_isalpha(text):
+#     # return ''.join(reversed([i if i.isalpha else dict.update(i) for i in text]))
+#     return ''.join(reversed([i for i in text if i.isalpha()]))
 
 
-def create_list(text):
-    return list(text.split(" "))
+# def make_anagram(text):
+#     for word in text.split(' '):
+#         print(word)
+#         return ''.join(reversed([i for i in word if i.isalpha()]))
 
+def make_revers(text):
+        rev_list_with_alpha = [letter for letter in text if letter.isalpha()]
+        rev_list_with_alpha.reverse()
+        for k, v in enumerate(num for num in (letter for letter in text)):
+            if not v.isalpha():
+                rev_list_with_alpha.insert(k, v)
+        return ''.join(rev_list_with_alpha)
 
 def make_anagram(text):
-    res = []
-    for i in create_list(text):
-        res.append(revers(i))
-    return ' '.join(res)
+    return ' '.join([make_revers(i) for i in text.split(" ")])
--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 E.g. "a1bcd efg!h" => "d1cba hgf!e"
 Use Latin alphabet for test only."""
 
-from anagram import make_anagram
+from anagram import make_anagram #, make_list_isalpha
 
 string = 'a1bcd efg!h lkjhdf7,j  zdf121aasf'
 print(f'String --> {string}')
 
 result = make_anagram(string)
 print(f'Result --> {result}')
+
+# text = 'lkjhdf7,j'
+#
+# print(make_list_isalpha(text))
 
 if __name__ == '__main__':
     cases = [
